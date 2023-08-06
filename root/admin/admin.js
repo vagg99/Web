e45488b3cdd2dcaa478a37d53ec0f44903f6639a
Nektar24 <michailidisnektarios@gmail.com>
2023-08-06 20:02:03 +0200

    testing

--- root/admin/admin.js
+++ root/admin/admin.js
@@ -71,24 +71,50 @@ function populateProducts() {
   }
 }
 
+const m = ["Ιανουαρίου", "Φεβρουαρίου", "Μαρτίου", "Απριλίου", "Μαΐου", "Ιουνίου", "Ιουλίου", "Αυγούστου", "Σεπτεμβρίου", "Οκτωβρίου", "Νοεμβρίου", "Δεκεμβρίου"];
+
+const d = new Date();
+let month = m[d.getMonth()];
 
 // Example data - replace this with your actual data
 const players = [
-  { name: 'Player 1', score: 100 },
-  { name: 'Player 2', score: 90 },
-  { name: 'Player 3', score: 85 },
-  { name: 'Player 4', score: 80 },
-  { name: 'Player 5', score: 70 },
-  { name: 'Player 6', score: 60 },
-  { name: 'Player 7', score: 50 },
-  { name: 'Player 8', score: 40 },
-  { name: 'Player 9', score: 30 },
-  { name: 'Player 10', score: 20 },
-  { name: 'Player 11', score: 10 },
-  { name: 'Player 12', score: 5 }
+  { name: 'Player 1', points: 100, "tokens": {"total": 100, "monthly": 0}},
+  { name: 'Player 2', points: 90, "tokens": {"total": 100, "monthly": 0} },
+  { name: 'Player 3', points: 85, "tokens": {"total": 100, "monthly": 0} },
+  { name: 'Player 4', points: 80, "tokens": {"total": 100, "monthly": 0} },
+  { name: 'Player 5', points: 70, "tokens": {"total": 100, "monthly": 0} },
+  { name: 'Player 6', points: 60, "tokens": {"total": 100, "monthly": 0} },
+  { name: 'Player 7', points: 50, "tokens": {"total": 100, "monthly": 0} },
+  { name: 'Player 8', points: 40, "tokens": {"total": 100, "monthly": 0} },
+  { name: 'Player 9', points: 30, "tokens": {"total": 100, "monthly": 0} },
+  { name: 'Player 10', points: 20, "tokens": {"total": 100, "monthly": 0} },
+  { name: 'Player 11', points: 10, "tokens": {"total": 100, "monthly": 0} },
+  { name: 'Player 12', points: 5, "tokens": {"total": 100, "monthly": 0} }
 ];
 
 const playersPerPage = 10;
+
+function fetchPlayersData() {
+  // Replace the URL with your actual backend endpoint to fetch player data
+  const url = 'https://your-backend-api.com/leaderboard';
+
+  return fetch(url)
+      .then(response => response.json())
+      .catch(error => {
+          console.error('Error fetching player data:', error);
+          return [];
+      });
+}
+
+async function refreshLeaderboard() {
+  try {
+      const players = await fetchPlayersData();
+      displayPlayers(1, players);
+      displayPagination(players.length);
+  } catch (error) {
+      console.error('Error refreshing leaderboard:', error);
+  }
+}
 
 function displayPlayers(page) {
   const leaderboardList = document.getElementById('leaderboard-list');
@@ -100,7 +126,7 @@ function displayPlayers(page) {
 
   playersToShow.forEach((player, index) => {
       const listItem = document.createElement('li');
-      listItem.innerHTML = `<span>${startIndex + index + 1}. ${player.name}</span><span>${player.score} points</span>`;
+      listItem.innerHTML = `<span>${startIndex + index + 1}. ${player.name}</span><span></span><span>${player.tokens["monthly"]} tokens ${month}</span><span>${player.tokens["total"]} Συνολικά tokens</span><span>${player.points} Συνολικοί Πόντοι</span>`;
       leaderboardList.appendChild(listItem);
   });
 }
@@ -122,6 +148,8 @@ function displayPagination() {
       paginationContainer.appendChild(pageLink);
   }
 }
+
+document.getElementById('refresh-button').addEventListener('click', refreshLeaderboard);
 
 // Initially display the first page of players
 displayPlayers(1);

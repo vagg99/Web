c64fc48d319497333216e6e85d30e7ab20474b63
Nektar24 <michailidisnektarios@gmail.com>
2023-08-15 16:47:11 +0200

    different color marker for current location

--- root/Maps/maps.js
+++ root/Maps/maps.js
@@ -1,5 +1,5 @@
-// Initialize the map with a default view at the specific location
-var map = L.map('map').setView([38.24511060644045, 21.7364112438391], 15); // Set the provided coordinates and zoom level 15
+// Initialize the map 
+var map = L.map('map')
 
 // Add the tile layer (OpenStreetMap as an example)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
@@ -12,7 +12,7 @@ function getUserLocation() {
       function(position) {
         const { latitude, longitude } = position.coords;
         map.setView([latitude, longitude], 15); // Set the view to user's location with zoom level 15
-        L.marker([latitude, longitude])
+        L.marker([latitude, longitude] , { icon: CurrectLocationIcon })
           .addTo(map)
           .bindPopup('You are here!') // Custom popup message for the user's location
           .openPopup();
@@ -42,8 +42,34 @@ async function displayAllStores(){
       .bindPopup(`<b>${name}</b>`)
       .openPopup();
   });
+  // Current Location
   getUserLocation();
+  // Patra
+  //map.setView([38.24511060644045, 21.7364112438391], 15); // Set the provided coordinates and zoom level 15
 }
+
+const CurrentLocationColor = "#40e0d0";
+
+function markerHtmlStyles(color) { return `
+  background-color: ${color};
+  width: 3rem;
+  height: 3rem;
+  display: block;
+  left: -1.5rem;
+  top: -1.5rem;
+  position: relative;
+  border-radius: 3rem 3rem 0;
+  transform: rotate(45deg);
+  border: 1px solid #FFFFFF`;
+}
+
+const CurrectLocationIcon = L.divIcon({
+  className: "current-location-pin",
+  iconAnchor: [0, 24],
+  labelAnchor: [-6, 0],
+  popupAnchor: [0, -36],
+  html: `<span style="${markerHtmlStyles(CurrentLocationColor)}" />`
+});
 
 // Initially display all stores
 displayAllStores();
